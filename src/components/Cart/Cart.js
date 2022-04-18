8328be03e0b3c2b504034e25ffab159621b33026
jibonghoshbd <jibonghosh1510@gmail.comm>
2022-04-19 02:34:17 +0600

    cart style done

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -10,13 +10,28 @@ const Cart = (props) => {
       } */
     const total = cart.reduce((previous, product) => (previous + product.price), 0)
 
+    let shippingCost;
+    if (total === 0) {
+        shippingCost = 0
+    } else {
+        shippingCost = 15;
+    }
+    const totalBeforeTax = total + shippingCost;
+    const tax = totalBeforeTax / 100;
+    const orderTotal = total + shippingCost + tax;
     return (
-        <div>
+        <div className='cart'>
             <h1>Order Summary</h1>
             <h3>Items Orders: {props.cart.length} </h3>
-            <div>
-                <p>Total: {total}</p>
+            <div className='price-ditels'>
+                <p>Items : ${total.toFixed(2)}</p>
+                <p>Shipping Cost : ${shippingCost.toFixed(2)}</p>
+                <p>Total Before Tax : ${totalBeforeTax.toFixed(2)}</p>
+                <p>Tax : ${tax.toFixed(2)}</p>
+                <h3 className='order-total'>Order Total : ${orderTotal.toFixed(2)}</h3>
+
             </div>
+            <button className='btn-regural'>Review your order</button>
         </div>
     );
 };
