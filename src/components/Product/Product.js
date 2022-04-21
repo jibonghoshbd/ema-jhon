7060693f8b5d6fe054118da0f3aad1cdef17ce19
jibonghoshbd <jibonghosh1510@gmail.comm>
2022-04-21 18:10:57 +0600

    react reating add

--- src/components/Product/Product.js
+++ src/components/Product/Product.js
@@ -2,13 +2,14 @@ import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faShoppingCart } from '@fortawesome/free-solid-svg-icons'
 import './Product.css';
+import Rating from 'react-rating';
 
 
 
 const Product = (props) => {
     // console.log(props.addProductToCart)
-    const { name, img, price, seller, stock } = props.product;
-
+    const { name, img, price, seller, stock, star } = props.product;
+    // console.log(props.product)
     const shoppingIcon = <FontAwesomeIcon icon={faShoppingCart} />
 
     return (
@@ -17,16 +18,30 @@ const Product = (props) => {
                 <img src={img} alt="" />
             </div>
             <div className='product-ditels'>
+
                 <h2 className='product-name'>Name: {name}</h2>
-                <p>By:{seller} </p>
-                <p>Price: ${price}</p>
-                <p>only {stock} left in stock - order soon</p>
+                <div className='reating-justify'>
+                    <span>
+                        <p>By:{seller} </p>
+                        <p>Price: ${price}</p>
+                        <p>only {stock} left in stock - order soon</p>
+                    </span>
+                    <span className='reating'>
+                        <Rating
+                            initialRating={star}
+                            readonly
+                            emptySymbol="far fa-star icon-color fa-2x"
+                            fullSymbol="fas fa-star icon-color fa-2x"
+                        ></Rating>
+                    </span>
+                </div>
+                <br />
                 <button
 
                     onClick={() => props.addProductToCart(props.product)}
                     className='btn-regural'>{shoppingIcon} add to cart</button>
-            </div>
 
+            </div>
         </div>
     );
 };
