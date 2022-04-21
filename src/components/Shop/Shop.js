3906713eca13cbd5d0395876efeb5b263fae0acc
jibonghoshbd <jibonghosh1510@gmail.comm>
2022-04-21 17:32:41 +0600

    serch products display

--- src/components/Shop/Shop.js
+++ src/components/Shop/Shop.js
@@ -6,12 +6,16 @@ import './Shop.css';
 const Shop = () => {
     const [products, setProducts] = useState([]);
     const [cart, setCart] = useState([])
+    const [displayProducts, setDisplayProducts] = useState([])
 
     useEffect(() => {
         const url = `https://raw.githubusercontent.com/ProgrammingHero1/ema-john-simple-resources/master/fakeData/products.JSON`;
         fetch(url)
             .then(res => res.json())
-            .then(data => setProducts(data))
+            .then(data => {
+                setProducts(data)
+                setDisplayProducts(data)
+            })
     }, []);
 
     useEffect(() => {
@@ -41,21 +45,36 @@ const Shop = () => {
         // add to local storage 
         addToDb(product.key)
     }
+
+    // handle search input 
+    const handelSearch = event => {
+        const searchText = event.target.value;
+        const matchedProduct = products.filter(product => product.name.toLowerCase().includes(searchText.toLowerCase()))
+        setDisplayProducts(matchedProduct);
+    }
+
     return (
-        <div className='shop-container'>
-            <div className="product-container">
-                {
-                    products.map(product => <Product
-                        key={product.key}
-                        product={product}
-                        addProductToCart={addProductToCart}
-                    ></Product>)
-                }
+        <>
+            <div
+                onChange={handelSearch}
+                className="search-container">
+                <input type="text" />
             </div>
-            <div className="cart-container">
-                <Cart cart={cart}></Cart>
+            <div className='shop-container'>
+                <div className="product-container">
+                    {
+                        displayProducts.map(product => <Product
+                            key={product.key}
+                            product={product}
+                            addProductToCart={addProductToCart}
+                        ></Product>)
+                    }
+                </div>
+                <div className="cart-container">
+                    <Cart cart={cart}></Cart>
+                </div>
             </div>
-        </div>
+        </>
     );
 };
 
